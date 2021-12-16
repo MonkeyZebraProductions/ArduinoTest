986528be0d8b2c9136113fa0edbacda6e4f5f7b2
DarraghSmithTUD <58596420+DarraghSmithTUD@users.noreply.github.com>
2021-12-16 14:57:41 +0100

    DarraghPush2

--- Arduino Project/Assets/Scripts/hamsterScoreDeath.cs
+++ Arduino Project/Assets/Scripts/hamsterScoreDeath.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class hamsterScoreDeath : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private SphereCollider playerCol;
+
+    public TextMeshProUGUI gameOver, scoreText;
+
+    public float score;
+
+
     void Start()
     {
-        
+        playerCol = GetComponent<SphereCollider>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        scoreText.text = "Score: " + score;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Hole")
+        {
+            gameOver.gameObject.SetActive(true);
+        }
+
+        if (other.gameObject.tag == "Carrot")
+        {
+            score += 1;
+        }
     }
 }
